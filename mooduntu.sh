1f94fecb8a6a93edb2ac9a079acc52b1e49c924d
Jerome Mouneyrac <jerome@mouneyrac.com>
2012-10-10 21:46:41 +0800

    start writing code for setting up mdk

--- mooduntu.sh
+++ mooduntu.sh
@@ -26,7 +26,7 @@ export DEBIAN_FRONTEND=noninteractive
 echo mysql-server mysql-server/root_password password moodle | sudo debconf-set-selections
 echo mysql-server mysql-server/root_password_again password moodle | sudo debconf-set-selections
 sudo apt-get -q -y install mysql-server
-sudo mysqladmin -u root password moodle
+#sudo mysqladmin -u root password moodle
 
 # PHP
 sudo apt-get --assume-yes install php5
@@ -35,11 +35,16 @@ sudo apt-get --assume-yes install php5-curl php5-gd php5-intl php5-xmlrpc
 # Restart apache
 sudo service apache2 restart
 
+# Install full mooduntu
+git clone git://github.com/mouneyrac/mooduntu.git ~/Documents/mooduntu
+
 # Moodle Dev Kit
 git clone git://github.com/FMCorz/mdk.git ~/Documents/MoodleDevKit
 chmod +x ~/Documents/MoodleDevKit/moodle
 chmod +x ~/Documents/MoodleDevKit/moodle-*.py
 sudo ln -s ~/Documents/MoodleDevKit/moodle /usr/local/bin
+cp ~/Documents/MoodleDevKit/config-dist.json ~/Documents/MoodleDevKit/config.json
+
 
 # Create a folder available from /var/www
 mkdir ~/Sites
@@ -50,6 +55,13 @@ sudo mkdir /home/www-data
 sudo mkdir /home/www-data/moodledata
 sudo chmod -R 0770 /home/www-data/moodledata
 sudo chown -R www-data /home/www-data
+
+# Setup Moodle Dev Kit
+#FILE=$HOME"/Documents/MoodleDevKit/config.json"
+#sed -i 's/"www": "\/var\/www"/"www": "\/var\/www\/Sites"/' $FILE
+
+
+# Install Moodle instances
 
 # Install Netbeans
 sudo apt-get -y install netbeans
