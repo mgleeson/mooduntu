b1bf7eee2e0dd142d4bb311b8c690999aaffb357
Jerome Mouneyrac <jerome@mouneyrac.com>
2012-10-16 22:29:26 +0800

    Install Google chrome stable

--- mooduntu.sh
+++ mooduntu.sh
@@ -164,3 +164,11 @@ echo "Installing Netbeans 7.2, please wait..."
 sudo bash /tmp/netbeans-linux.sh --silent
 sudo ln -s /usr/local/netbeans-7.2/bin/netbeans /usr/local/bin/netbeans
 
+# Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update 
+sudo apt-get install -y google-chrome-stable
+
+
+
